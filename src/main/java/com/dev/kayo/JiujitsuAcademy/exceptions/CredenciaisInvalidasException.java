f3d0ab60ecfc27c6b462392244dbff8caddc3950
Kayo123451 <kayousouza11@gmail.com>
2026-07-21 10:54:26 -0300

    Feat: Implementa exception customizadas para validar credenciais

---
+++ src/main/java/com/dev/kayo/JiujitsuAcademy/exceptions/CredenciaisInvalidasException.java
@@ -0,0 +1,9 @@
+package com.dev.kayo.JiujitsuAcademy.exceptions;
+
+public class CredenciaisInvalidasException extends RuntimeException {
+
+    public CredenciaisInvalidasException(String message) {
+        super(message);
+    }
+
+}
---
+++ src/main/java/com/dev/kayo/JiujitsuAcademy/exceptions/UsuarioJaCadastradoException.java
@@ -0,0 +1,9 @@
+package com.dev.kayo.JiujitsuAcademy.exceptions;
+
+public class UsuarioJaCadastradoException extends RuntimeException {
+
+    public UsuarioJaCadastradoException(String message) {
+        super(message);
+    }
+
+}
